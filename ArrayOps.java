2981aed7fc60378c450eea36da6e59691a4cd882
loren.kaso <loren.kaso@post.runi.ac.il>
2024-01-15 01:51:56 +0200

    test 1.2

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,13 +1,14 @@
 public class ArrayOps {
     public static void main(String[] args) {
-       int [] arr = {0,1,4,53,25,2};
+    //    int [] arr = {0,1,4,53,25,2};
     //   System.out.println(findMissingInt(arr));
-       System.out.println(secondMaxValue(arr));
-       System.out.println(isSorted(new int[] {7,7, 5, 4, 3, -12})); // true
-      System.out.println( isSorted(new int[] {1, 2, 3})); // true
-      System.out.println( isSorted(new int[] {1, -2, 3})); // false
-      System.out.println( isSorted(new int[] {1, 1, 500})); // true
-      System.out.println( isSorted(new int[] {1, 3, 2})); // false
+    System.out.println(containsTheSameElements(new int[] {3, -4, 1, 2, 5}, new int[] {1, 3, -4, 5}));;
+    //    System.out.println(secondMaxValue(arr));
+    //    System.out.println(isSorted(new int[] {7,7, 5, 4, 3, -12})); // true
+    //   System.out.println( isSorted(new int[] {1, 2, 3})); // true
+    //   System.out.println( isSorted(new int[] {1, -2, 3})); // false
+    //   System.out.println( isSorted(new int[] {1, 1, 500})); // true
+    //   System.out.println( isSorted(new int[] {1, 3, 2})); // false
        
     }
     
@@ -44,10 +45,56 @@ public class ArrayOps {
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        int [] uniq1 = set(array1);
+        int [] uniq2 = set(array2);
+        boolean isSame = false;
+        if(uniq1.length != uniq2.length){
+            return isSame;
+        }
+        for (int i = 0; i < uniq1.length; i++) {
+            for (int j = 0; j < uniq2.length; j++) {
+                if(array1[i] == array2[j]){
+                    isSame = true;
+                    break;
+                }
+            }
+            if(!isSame){
+                return isSame = false;
+            }
+        }
+        return isSame;
     }
 
+    /********helper function*******/
+    public static boolean contains(int[] arr, int value, int index) {
+        for (int i = 0; i < index; i++) {
+            if (arr[i] == value) {
+              return true;
+            }
+        }
+        return false ;
+    }
+        
+    public static int countUnique(int[] arr) {
+        int count = 0; 
+        for (int i = 0; i < arr.length; i++) {
+            count = contains(arr, arr[i], i) ? count : count + 1;
+        }
+        return count;
+    }
+        
+    public static int [] set (int[] arr) {
+        int [] res = new int [countUnique(arr)];
+        int index = 0; 
+        for (int i = 0; i < arr.length; i++) {
+            if (!contains(arr, arr[i], i)){
+                res[index++] = arr[i];
+            }
+        }
+        return res;
+    }
+        
+    /*****************************************/
 
     public static boolean isSorted(int [] array) {
         boolean isSorted = true ;
